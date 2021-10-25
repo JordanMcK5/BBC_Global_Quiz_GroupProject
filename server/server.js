98f01b4190fd57f726df9518d8a86b38f4b2f2ac
Athina Gouseti <athina.gouseti@outlook.com>
2021-10-25 13:32:43 +0100

    Added back end routers

--- server/server.js
+++ server/server.js
@@ -1,23 +1,27 @@
 const express = require('express');
 const app = express();
-const MongoClient = require('mongodb').MongoClient;
-const createRouter = require('./helpers/create_router');
-const cors = require('cors');
+const cors =require('cors');
+const MongoClient = require('mongodb').MongoClient
+const createRouter = require('./helpers/create_router.js');
 
-app.use(cors())
 app.use(express.json())
+app.use(cors())
 
-MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true }, (err, client) => {
-  if(err){
-    console.log(err);
-  }
-
-  const db = client.db('world');
-  const countriesCollection = db.collection('countries');
-  const countriesRouter = createRouter(countriesCollection)
-  app.use('/api/countries', countriesRouter);
-
-  app.listen(5000, function(){
-    console.log(`app listening on port ${this.address().port}`);
-  })
+MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true})
+.then((client) => {
+    const db = client.db('world')
+    const countriesCollection = db.collection('countries')
+    const countriesRouter = createRouter(countriesCollection)
+    const flagsQuizCollection = db.collection('flags-quiz')
+    const flagsQuizResultsRouter = createRouter(flagsQuizCollection)
+    const capitalsQuizCollection = db.collection('capitals-quiz')
+    const capitalsQuizResultsRouter = createRouter(capitalsQuizCollection)
+    app.use('/api/countries', countriesRouter)
+    app.use('/api/countries/quiz-results/flags', flagsQuizResultsRouter)
+    app.use('/api/countries/quiz-results/capitals', capitalsQuizResultsRouter)
 })
+.catch(console.error)
+
+app.listen(5000, function() {
+    console.log(`Listening on port ${ this.address().port }`)
+});
