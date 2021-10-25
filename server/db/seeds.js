80b62d562c54e46bf6b2595a96ea8bb3daf15010
Athina Gouseti <athina.gouseti@outlook.com>
2021-10-25 22:37:16 +0100

    hard coded a user's score, but left as comment

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -7,4 +7,69 @@ db.countries.insertMany([
     language:"French"
     }
 
+    // {
+    //     name: "Ted",
+    //     results: [
+    //         {
+    //             0: {
+    //                 round: 1,
+    //                 winner: true
+    //             }
+    //         },
+    //         {
+    //             1: {
+    //                 round: 2,
+    //                 winner: true
+    //             }
+    //         },
+    //         {
+    //             2: {
+    //                 round: 3,
+    //                 winner: true
+    //             }
+    //         },
+    //         {
+    //             3: {
+    //                 round: 4,
+    //                 winner: true
+    //             }
+    //         },
+    //         {
+    //             4: {
+    //                 round: 5,
+    //                 winner: false
+    //             }
+    //         },
+    //         {
+    //             5: {
+    //                 round: 6,
+    //                 winner: true
+    //             }
+    //         },
+    //         {
+    //             6: {
+    //                 round: 7,
+    //                 winner: true
+    //             }
+    //         },
+    //         {
+    //             7: {
+    //                 round: 8,
+    //                 winner: true
+    //             }
+    //         },
+    //         {
+    //             8: {
+    //                 round: 9,
+    //                 winner: false
+    //             }
+    //         },
+    //         {
+    //             9: {
+    //                 round: 10,
+    //                 winner: true
+    //             }
+    //         }
+    //     ]
+    // }
 ]);
