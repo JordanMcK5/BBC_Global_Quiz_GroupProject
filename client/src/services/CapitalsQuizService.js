287fdf9ca43db05a180a6ef801776b76dc270074
Athina Gouseti <athina.gouseti@outlook.com>
2021-10-25 13:35:21 +0100

    Added service for capitals

---
+++ client/src/services/CapitalsQuizService.js
@@ -0,0 +1,19 @@
+export const getResults = () => {
+    return fetch('http://localhost:5000/api/countries/quiz-results/capitals')
+    .then(res => res.json())
+}
+
+export const postResults = (payload) => {
+    return fetch('http://localhost:5000/api/countries/quiz-results/capitals', {
+    method: 'POST',
+    body: JSON.stringify(payload),
+    headers: { 'Content-type': 'application/json' }
+ })
+ .then(res => res.json())
+}
+
+export const deleteBooking = (id) => {
+    return fetch('http://localhost:5000/api/countries/quiz-results/capitals' + id, {
+        method: 'DELETE'
+    })
+};
