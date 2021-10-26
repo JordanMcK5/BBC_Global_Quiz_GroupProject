7d64921a3a4c72056e9e7b56006026f0f74d4763
Jordan McKenna <jordanmckenna@JORDANs-MacBook-Pro.local>
2021-10-26 16:26:19 +0100

    updated flags quiz css

--- client/src/components/FlagsQuizQuestion.js
+++ client/src/components/FlagsQuizQuestion.js
@@ -8,12 +8,15 @@ const FlagsQuizQuestion = ({currentCountry, answers, receiveAnswer }) => {
 
     const quizAnswers = answers.map((answer) => {
         return (
+            <>
             <img className="flag-quiz-images" key={answer.abbreviation} alt="flag-img" src={answer.flag} onClick={() => { receiveAnswer(answer.abbreviation) }} />
+            </>
         )
     })
 
     return (
         <div>
+                    <img className="flag_logo" height="250" src={`${process.env.PUBLIC_URL}/flag.png`}  />
             <p className="capital1">What is the flag of {currentCountry}?</p>
             <div id="answers">
                 {quizAnswers}
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -11,7 +11,7 @@ const NavBar = () => {
                 <Link to="/">Home</Link>
             </li>
             <li>
-                <Link to="/map">View map</Link><br></br>
+                <Link to="/map">View map</Link>
             </li>
             <li>
                 <Link to="/quiz/national-animals">National Animals Quiz</Link>
