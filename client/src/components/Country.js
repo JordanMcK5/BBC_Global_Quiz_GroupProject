9a4de30c38da0758536c5b04822f81464779377a
AS-Brown <andrewsbrown@hotmail.co.uk>
2021-10-27 11:20:17 +0100

    Fixed map bug

--- client/src/components/Country.js
+++ client/src/components/Country.js
@@ -8,8 +8,13 @@ const Country = ({ countries }) => {
 
     var countryName = window.location.href.replace("http://localhost:3000/country/?detail=", "")
     var countryFinal = countryName.replaceAll("%20", " ")
+    var countryFinal2 = countryFinal.replaceAll("%C3%A7", "ç")
+    var countryFinal3 = countryFinal2.replaceAll("%C3%85", "Å")
+    var countryFinal4 = countryFinal3.replaceAll("%C3%A9", "é")
+    
 
     const countryFind = function (countryList, fixedName) {
+        console.log(fixedName);
         for (var country of countryList) {
             if (country.name === fixedName) {       
                 return country
@@ -17,7 +22,7 @@ const Country = ({ countries }) => {
         }
     }
 
-    const countryDetails = countryFind(countries, countryFinal)
+    const countryDetails = countryFind(countries, countryFinal4)
 
     const languagesSpoken = countryDetails?.languages.map((language) => {
         return language.name
